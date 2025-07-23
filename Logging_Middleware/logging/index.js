24c17c4226b8c773e96714318cbe7bdab509abc7
BVaishnavi15 <bijjula.vaishnavi15@gmail.com>
2025-07-23 12:49:34 +0530

    first commit

---
+++ Logging_Middleware/logging/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors = require('cors')
+const app=express()
+const PORT=5000
+app.use(express.json())
+
+let api=[]
+users=http://20.244.56.144/evaluation-service/logs
+ api.post('/p',(req,res))={
+    const { stack, level, package, message } = req.body
+  users.push({ id: Date.now(), stack, level, package, message })
+  res.status(201).json({ message: 'Registered successfully' })
+});
+ 
+ 
+
